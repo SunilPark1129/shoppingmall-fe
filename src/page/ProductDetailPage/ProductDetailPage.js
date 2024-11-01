b9d49b04f72eb5545234dd681a217a401113eeff
Sunil Park <sunilpark1129@gmail.com>
2024-11-01 03:55:14 -0400

    edited - currency sign

--- src/page/ProductDetailPage/ProductDetailPage.js
+++ src/page/ProductDetailPage/ProductDetailPage.js
@@ -108,7 +108,7 @@ const ProductDetail = () => {
         <Col className="product-info-area" sm={6}>
           <div className="product-info">{selectedProduct.name}</div>
           <div className="product-info">
-            ₩ {currencyFormat(selectedProduct.price)}
+            $ {currencyFormat(selectedProduct.price)}
           </div>
           <div className="product-info">{selectedProduct.description}</div>
 
