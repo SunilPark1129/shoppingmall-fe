bd7e5c2c43f6e6d316f47980987a10e08bf20fe2
Sunil Park <sunilpark1129@gmail.com>
2024-11-05 10:00:31 -0500

    added - copyright policy

--- src/common/component/Footer.js
+++ src/common/component/Footer.js
@@ -3,7 +3,17 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <div className="wrapper">2024 Shoppingmall Project - spark</div>
+      <div className="wrapper">
+        <div className="copyright">
+          This website is a project that simulates an online shopping mall, and{" "}
+          <strong>purchasing items is not possible</strong>. The photos used on
+          this website do not represent items actually sold here. All images,
+          except for those used in the banners, are{" "}
+          <strong>sourced from H&M products</strong>, and no names, information,
+          or images have been altered.
+        </div>
+        <div>2024 Shoppingmall Demo Project - spark</div>
+      </div>
     </footer>
   );
 }
